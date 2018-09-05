e3e46369a84d951b09e6785dea9a351b447ebbdd
Ben B <bbuckwalter@huskers.unl.edu>
2018-09-05 15:40:31 -0500

    Added name and adjusted style a bit

---
+++ Hello.c
@@ -0,0 +1,15 @@
+ /**
+  *Author: Kathleen Kirshenbaum and Ben Buckwalter
+  *Date: 9/5/18
+  *Hello world program
+  */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Kathleen Kirshenbaum\n");
+  printf("Ben Buckwalter\n");
+  return 0;
+}
